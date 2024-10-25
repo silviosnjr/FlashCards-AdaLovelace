a86c8e263de00c743ed89143e3ef281017097820
silviosnjr <silviosnjr@gmail.com>
2024-10-25 10:14:07 -0300

    first commit

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Ciências', 
+    'O que é fotossíntese?', 
+    'Fotossíntese é o processo pelo qual as plantas produzem energia usando luz solar'
+)
+
+criaCartao(
+    'História', 
+    'Quem foi Mahatma Gandhi?', 
+    'Gandhi foi um líder indiano que lutou pela independência usando a não-violência'
+)
+
+criaCartao(
+    'Filosofia', 
+    'O que é ética?', 
+    'Ética é o estudo do que é moralmente certo ou errado'
+)
+
+criaCartao(
+    'Matemática', 
+    'O que é uma fração?', 
+    'Fração é uma forma de representar uma parte de um todo'
+)
+
+criaCartao(
+    'Geografia', 
+    'O que é um continente?', 
+    'Um continente é uma grande massa de terra cercada por oceanos'
+)
+
+criaCartao(
+    'Astronomia', 
+    'O que são constelações?', 
+    'Constelações são agrupamentos de estrelas que formam figuras no céu'
+)
+
+criaCartao(
+    'Literatura', 
+    'Quem escreveu A Odisséia?', 
+    'A Odisséia foi escrita pelo poeta grego Homero'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é HTML?', 
+    'HTML é uma linguagem de marcação usada para criar páginas web'
+)
