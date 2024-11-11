afb4d26e4aa74818459750aabde22ccd91c0f0d1
silviosnjr <silviosnjr@gmail.com>
2024-11-11 04:03:14 -0300

    ajustes

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>categoria</h3>
-    <div class="cartao__conteudo__pergunta">
-        <p>pergunta</p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-        <p>resposta</p>
-    </div>
-    </div>
+        <div class="cartao__conteudo">
+        <h3>categoria</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>pergunta</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>resposta</p>
+        </div>
+        </div>
     `
 
     let respostaEstaVisivel = false
@@ -23,6 +23,5 @@ function criaCartao(categoria, pergunta, resposta){
     }
     cartao.addEventListener('click', viraCartao)
     
-
     container.appendChild(cartao)
 }
